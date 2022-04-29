3a30f8ea28eebf28bd65a6ad1698eb0532e90fea
Manaheeljamil <manaheeljamil20@gmail.com>
2022-04-29 11:09:36 -0700

    First commit

---
+++ note.js
@@ -0,0 +1,214 @@
+               //  (CHAPTER NO 1 "ALERT") :
+ 
+                    // Question # 1 :
+                        
+// alert("Welcome! Manaheel jamil Assignment.");
+                     // Question # 2 :
+                        
+// alert("Error! Please enter a valid  password.");
+                     // Question # 3 :
+
+// alert("Welcome to JS Land \n Happy Coding!");
+                     // Question # 4 :
+
+// alert("Welcome to JS Land...");
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs.");
+                    // Question # 5 :
+
+// alert("Hello...I can run JS through my web browser's console");
+                    // Question # 6 :
+
+{/* <script>alert("Javascript work in Html file")</script> */}
+                    // Question # 7 :
+                 
+// The true way of script file placing is "Body(after your page's HTML.)"
+     
+                // (CHAPTER NO 2 "VARIABLE FOR STRINGS" ):
+
+                     // Question # 1 :
+// var userName;
+                     // Question # 2 :  
+// var myName = "Manaheel Jamil";
+
+                     // Question # 3 :
+// [a]  var message;           
+// [b]  message = "Hello World";
+// [c]  alert(message);
+ 
+                      //  Question # 4 :
+// var  userName = "Manaheel Jamil";
+// var age = "19 years old";
+// var Qualification = "under Graduate";
+// alert(userName);
+// alert(age);
+// alert(Qualification);
+
+                       // Question # 5 :
+// var JS = "PIZZA \n PIZZ  \n PIZ \n PI \n P";
+// alert(JS);
+ 
+                       // Question # 6 :
+// var email = "manaheeljamil20@gmail.com";
+// alert("My email address is" +" " + email);
+
+                       // Question # 7 :
+// var book = "A smarter \n way to learn javascript";
+// alert("I am trying to learn from the Book" + " " + book);
+
+                        // Question # 8 :
+// var display = "Yah! I can write HTML content through JavaScript";
+// document.write(display);
+
+                         // Question # 9 :
+// var string = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(string);
+
+                 // (CHAPTER NO 3 "VARIABLES FOR NUMBERS");
+                         //  Question # 1 :
+// var age = "19";
+// alert("I am" + " " + age + " "  + "years old.");
+
+                         // Question # 2 :
+// var visitor = prompt("How many time you visited a webpage?");
+// alert("You have visited this site" + " " + visitor + " " + "times.");
+
+//                          // Question # 3 :
+//  var birthYear = "2002";
+//  var userName = "my birth year is"+ birthYear;
+//  document.write("My birth Year is " + " " + birthYear + "<br/>" + "Data type of my declared variable is number");
+
+                          // Question # 4 :
+// var visitorName = prompt("What is your Name?");
+// var productTitle = prompt("What store you selected?");
+// var Quantity = prompt("How many product wants to order?");
+// alert(visitorName + " " + "ordered" + " " + Quantity + " " + "on" + " " + productTitle);
+
+                 // (CHAPTER NO 4 "VARIABLE NAMES: LEGAL & ILLEGAL") ;
+                          //  Question # 1 :
+// var name = "manaheel";var fatherName = "jamil";var age = "19 years old";
+    
+                          // Question # 2 :
+// // legal variable
+// var legalVariable = "Start with a letter,Use _ (underscores) for clarity,Can use numbers,Don't use special symbols,Don't have spaces" 
+
+                          // Question # 3 :
+// document.write("<strong>Rules for Naming JS variable<strong/>")      
+// var varName = "Variable name can only contain,numbers,$ and_. Foe example : $my_1st Variables."
+// var second =  "Variable must begin with a letter, $or _. For example : $name, _name or name."
+// var sensitive =  "Variables are case sensitive."
+// var keyWords = "Variables name should not be JS keywords."
+// document.write("<br/>" + varName + "<br/>" + second + "<br/>" + sensitive + "<br/>" + keyWords)
+
+                // (CHAPTER NO 5 :"MATH EXPRESSION")
+                         // Question # 1 :
+// var sum = 4 + 17;
+// document.write("Sum of 4 and 17 is" + " " + sum)
+
+                         //  Question # 2 :
+           // FOR SUBTRACTION:
+// var subtraction = 17 - 4;
+// document.write("Subtraction of 17 and 4 is" + subtraction)
+           // FOR MULTIPLICATION:
+// var multiplication = 17 * 4;
+// document.write("Multiplication of 17 and 4 is" + " " +  multiplication)
+            // FOR DIVISION:
+// var division = 17 / 4;
+// document.write("Division of 17 and 4 is" + " " + division)
+             // FOR MODULUS:
+// var modulus = 17 % 4;
+// document.write("Modulus of 17 and 4 is" + " " + modulus)
+
+                         // Question # 3:
+
+//  var userName;
+// var i = "5";
+// var incr= i + 1;
+// var add = incr + 7;
+// var decr = add - 1;
+// var divide = decr / 3;
+// document.write("Value after variable declaration is" + userName + "<br/>" + "Initial value is :" + i + "<br/>"+ "Value after increament is :" + incr + "<br/>"
+// + "Value after addition is : " + add + "<br/>" + "Value after decreament is : " + decr + "<br/>" + "The remainder is : " +
+// divide )
+
+
+                          // Question # 4:
+// var ticketPrice = "600";
+// var totalCost = ticketPrice * "5";
+// document.write("Total cost to buy 5 ticket to a movie is" + totalCost);
+
+                         //  Question # 5:
+// var name = 2;
+// document.write("TABLE OF 2" +"<br/>" + "<br/>")
+// document.write(name + " " + "*" + " " + 1 + " "+ "=" + " " + name * 1 + "<br/>")
+// document.write(name + " " + "*" + " " + 2 + " "+ "=" + " " + name * 2 + "<br/>")
+// document.write(name + " " + "*" + " " + 3 + " "+ "=" + " " + name * 3 + "<br/>")
+// document.write(name + " " + "*" + " " + 4 + " "+ "=" + " " + name * 4 + "<br/>")
+// document.write(name + " " + "*" + " " + 5 + " "+ "=" + " " + name * 5 + "<br/>")
+// document.write(name + " " + "*" + " " + 6 + " "+ "=" + " " + name * 6 + "<br/>")
+// document.write(name + " " + "*" + " " + 7 + " "+ "=" + " " + name * 7 + "<br/>")
+// document.write(name + " " + "*" + " " + 8 + " "+ "=" + " " + name * 8 + "<br/>")
+// document.write(name + " " + "*" + " " + 9 + " "+ "=" + " " + name * 8 + "<br/>")
+// document.write(name + " " + "*" + " " + 10 + " "+ "=" + " " + name * 10 + "<br/>")
+
+
+
+
+                          // Question # 6:
+// var celsiusTemp = "25";
+// farenhit = (celsiusTemp * 9/5) + 32;
+// var farenTemp = "70";
+// celsius = ((70 - 32)* (5 / 9));
+// document.write("25°C IS " + " " + farenTemp + "°F" +"<BR/>" + "70°C is" + celsius + "°C")
+
+
+                          // Question # 7:
+// var price1 = 650;
+// var quantity1 = 650 * 3;
+// var price2 = 100;
+// var quantity2 = 100 * 7;
+// var shippingCharg = 100;
+// var total = quantity1 + quantity2 + shippingCharg;
+// var one = "SHOPPING CART" + "<br/>" + "<br/>" + "Price of item 1 is 650" + "<br/>" + "Quantity of item 1 is 3" + "<br/>" + 
+// "Price of item 2 is 100" + "<br/>" + "Quantity of item 2 is 7" + "<br/>"  + "Shipping Charges" + "<br/>" + "<br/>" + "Total cost of your order is" + " "+ total;
+// document.write(one)
+
+                           // Question # 8 :
+// var obtainedMarks = "804";
+// var totalMarks = "980";
+// var percentage = (obtainedMarks / totalMarks) * 100;
+// var two = "Total Marks:"+ " " + totalMarks + "<br/>" + "Obtained Marks:" + " " + obtainedMarks + "<br/>" + "Percentage" + " " + percentage;
+// document.write("MARK SHEET" + "<BR/>" + "<BR/>" + two)
+
+                            // Question # 9:
+
+// var totalCurency = ("104.28" * "10") + ("28" * "25");
+// document.write(" Currency in PKR" + "<BR/>" + "<BR/>" + "Total Currency in PKR:" + " " + totalCurency)
+
+                             // Question # 10:
+
+// var num = (((17 + 5) * 10) / 2); 
+
+                             // Question # 11:
+// var currentYear = 2022;
+// var birthYear = 2002;
+// var age = 2022 - 2002;
+// document.write("Current Year is:" + " " + currentYear + "<br/>" + "Birth Yearis:" + " " + birthYear + "<br/>" + "Your Age is:" + " " + age)
+
+//                             // Question # 12:
+// var radius = 20;
+// var circum = ((2 * 3.142) * 20);
+// var area = (3.142 * (20 * 20));
+// document.write("The Geometrizer" + "<br/>"+ "<br/>" + "Radius of a circle is:" + radius + "<br/>" + "The Circumference is:" + " " + circum + "<br/>" + "The Area is:" + " " + area)
+
+                               // Question # 13:
+// var favoSnack = "Lays";
+// var currentAge = "19";
+// var maxAge = "70";
+// var amount = "2";
+// var totalSnack = ((70 - 19) * 2);
+// document.write("You will need " + " " + totalSnack + " " + favoSnack + " " + "to last you until the ripe old age of" + " " + maxAge)
+
+
+
+
+
